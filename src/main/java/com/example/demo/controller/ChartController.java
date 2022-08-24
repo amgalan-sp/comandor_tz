b047f2b3a3d50737ddf660593633705a41117df2
amg <darzhaev@gmail.com>
2022-08-25 07:14:39 +0900

    правка по доп.окну

--- src/main/java/com/example/demo/controller/ChartController.java
+++ src/main/java/com/example/demo/controller/ChartController.java
@@ -10,14 +10,17 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -43,6 +46,8 @@ public class ChartController implements Initializable  {
     @FXML
     private TextField searchBar;
     @FXML
+    private TextField searchBarToPay;
+    @FXML
     private Good currentFood;
     @FXML
     private ListView<Good> products;
@@ -62,27 +67,53 @@ public class ChartController implements Initializable  {
     private TableColumn<Checks, Double> totalSum;
 
     @FXML
-    private Button button;
+    private Button buttonToPay;
     @Value("classpath:/bankSecurePay.fxml")
     private Resource chartResource;
+// используем преимущества спринга
 
     public ChartController(GoodRepository goodRepository, ChecksRepository checksRepository) {
         this.goodRepository = goodRepository;
         this.checksRepository = checksRepository;
     }
-
+// Соответсвующая поисковой строке продуктов функция
     @FXML
     void search(KeyEvent event) {
         products.getItems().clear();
         products.getItems().addAll(searchList(searchBar.getText(),findAll()));
     }
+// Создание нулевого чека
     Checks newCheck = new Checks(LocalDate.now(), LocalTime.now(),  0);
 
+    @FXML
+    void comparise(KeyEvent event) {
+    }
+// добавление модального окна
+    public void showDialog(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(chartResource.getURL());
+            Parent parent =loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(parent));
+            stage.setMinHeight(150);
+            stage.setMinWidth(300);
+            stage.setResizable(false);
+            stage.setTitle("Окно оплаты");
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow());
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+//  Ввод списка товаров в из БД
         products.getItems().addAll(findAll());
+//   Добавление нулевого чека для корректного добавления списка в корзине
         checkList.add(newCheck);
+//   Единоразовое добавление кандидата - Нового чека на покупки
         totalCartTable.setItems(checkList);
         products.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Good>() {
 
@@ -92,25 +123,31 @@ public class ChartController implements Initializable  {
                 int count =1;
 
                 currentFood = products.getSelectionModel().getSelectedItem();
+// Баг с добавлением пустого товара
                 if (currentFood!=null) {
+// При нулевом списке
                     if (shoppingList.size() == 0) {
                         shoppingList.add(new Checklines(newCheck, currentFood, lineNumber + 1, count, (count * currentFood.getPrice())));
                     } else {
+// Поиск такой же позиции  в корзине и определение кол-ва и суммы
                         shoppingList.stream().filter(checklines ->
                                 checklines.getGood().equals(currentFood)).forEach(
                                 e -> {
                                     e.setCount(e.getCount() + 1);
                                     e.setSum(e.getCount() * e.getGood().getPrice());
                                 });
+// При условии отсутствия данного  товара в корзине добавляется в корзину
                         if (shoppingList.stream().noneMatch(checklines ->
                                 checklines.getGood().equals(currentFood))) {
                             shoppingList.add(new Checklines(newCheck, currentFood, lineNumber + 1, count, (count * currentFood.getPrice())));
                         }
                     }
                 }
+//  Определение ячеек
                 name.setCellValueFactory(new PropertyValueFactory<Checklines, String>("good"));
                 countOfCheckLine.setCellValueFactory(new PropertyValueFactory<Checklines, Integer>("count"));
                 sum.setCellValueFactory(new PropertyValueFactory<Checklines, Double>("sum"));
+// Удаление позиций
                 delete.setCellFactory(ActionButtonTableCell.<Checklines>forTableColumn("Удалить", (Checklines p) -> {
                     shoppingCartTable.getItems().remove(p);
                     newCheck.setSum(shoppingCartTable.getItems().stream().map(Checklines::getSum).reduce(Double::sum).orElse(0.0));
@@ -118,31 +155,19 @@ public class ChartController implements Initializable  {
                     shoppingCartTable.refresh();
                     return p;
                 }));
-
+// Добавление списка товаров в отображаемый список
                 shoppingCartTable.setItems(shoppingList);
+// Расчет итоговой суммы
                 newCheck.setSum(shoppingCartTable.getItems().stream().map(Checklines::getSum).reduce(Double::sum).orElse(0.0));
+//  Определение ячейки
+
                 totalSum.setCellValueFactory(new PropertyValueFactory<Checks,Double>("sum"));
                 totalCartTable.refresh();
                 shoppingCartTable.refresh();
-                button.setOnAction(event -> {
-                    button.getScene().getWindow().hide();
-
-                    try {
-                        FXMLLoader loader = new FXMLLoader(chartResource.getURL());
-                        Parent parent =loader.load();
-                        Stage stage = new Stage();
-                        stage.setScene(new Scene(parent));
-                        stage.setTitle("Окно оплаты");
-                        stage.showAndWait();
-
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                });
             }
         } ) ;
     }
+// Поиск товаров по введенному в поисковой строке симоволов по совпадению начального ввода
     private List<Good> searchList(String searchWords, List<Good> listOfStrings) {
 
         List<String> searchWordsArray = Arrays.asList(searchWords.trim().split(" "));
@@ -155,6 +180,7 @@ public class ChartController implements Initializable  {
     private List<Good> findAll(){
         return goodRepository.findAll();
     }
+//  функция записи чека в БД
     private Checks createCheck(Checks check){
         return checksRepository.saveAndFlush(check);
     }
