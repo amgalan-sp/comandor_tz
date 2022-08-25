3167d358473249ee53b11444dc9d2abc153e76a3
amg <darzhaev@gmail.com>
2022-08-26 05:57:56 +0900

    fixed code in  Init

--- src/main/java/com/example/demo/init/Init.java
+++ src/main/java/com/example/demo/init/Init.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class Init {
@@ -17,17 +19,15 @@ public class Init {
     }
     @PostConstruct
     private void postConstruct(){
-        Good appleKrasnodarskie = new Good(1,"Яблоки краснодарские",98.7);
-        Good appleRedChief = new Good(2,"Яблоки Ред  Чиф",99.5);
-        Good tangerines = new Good(3,"Мандарины",124.5);
-        Good pearsForell = new Good(4,"Груши Форелл",124.5);
-        Good pearsPackham = new Good(5,"Груши Пакхам",99.5);
-        Good juiceCocaCola05 = new Good(6,"Coca-Cola  0,5 л",95.0);
-        goodRepository.saveAndFlush(appleKrasnodarskie);
-        goodRepository.saveAndFlush(appleRedChief);
-        goodRepository.saveAndFlush(tangerines);
-        goodRepository.saveAndFlush(pearsForell);
-        goodRepository.saveAndFlush(pearsPackham);
-        goodRepository.saveAndFlush(juiceCocaCola05);
+        List<Good> goodList = new ArrayList<>();
+        goodList.add(new Good(1,"Яблоки краснодарские",98.7));
+        goodList.add(new Good(2,"Яблоки Ред  Чиф",99.5));
+        goodList.add(new Good(3,"Мандарины",124.5));
+        goodList.add(new Good(4,"Груши Форелл",124.5));
+        goodList.add(new Good(5,"Груши Пакхам",99.5));
+        goodList.add(new Good(6,"Coca-Cola  0,5 л",95.0));
+        goodList.forEach(good -> goodRepository.saveAndFlush(good));
+
+
     }
 }
